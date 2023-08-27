97148d3355706e338ce11e00067e3fe94ba7d4d4
victorvieiradev <contatovictorvieira@outlook.com>
2023-08-26 22:18:20 -0300

    start

---
+++ src/main/kotlin/com/jornadamilhas/apijornadamilhas/ApiJornadamilhasApplication.kt
@@ -0,0 +1,11 @@
+package com.jornadamilhas.apijornadamilhas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiJornadamilhasApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiJornadamilhasApplication>(*args)
+}
---
+++ src/test/kotlin/com/jornadamilhas/apijornadamilhas/ApiJornadamilhasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jornadamilhas.apijornadamilhas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiJornadamilhasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
